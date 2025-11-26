346a3c36a8656c123cd86d1ef269391ce5a3a44b
MateuszCzz <mateuszczernic.mc@gmail.com>
2025-11-26 20:14:04 +0100

    Remove id functionality from songStore it was moved to backend

--- src/stores/song-store.ts
+++ src/stores/song-store.ts
@@ -4,17 +4,13 @@ import type { SongType } from "../types/song-type";
 
 export const useSongStore = defineStore('songStore', () => {
     const songs = ref<SongType[]>([]);
-    // id increment
-    const nextId = ref<number>(0);
 
     function addSong(song: SongType) {
-        song.id = nextId.value++;
         songs.value.push(song);
     }
 
     function addSongs(songsArray: SongType[]) {
         songsArray.forEach(song => {
-            song.id = nextId.value++;
             songs.value.push(song);
         });
     }
