b2141fb4907e80eea936442f95f2ec0600c32b24
MateuszCzz <mateuszczernic.mc@gmail.com>
2025-11-26 18:41:10 +0100

    Change frontend song type

--- src/types/song-type.ts
+++ src/types/song-type.ts
@@ -1,17 +1,30 @@
+export enum Mode {
+  Standard = 0,
+  Taiko = 1,
+  CatchTheBeat = 2,
+  Mania = 3,
+}
+
+export enum RankedStatus {
+  Unknown = 0,
+  Unsubmitted = 1,
+  PendingWipGraveyard = 2,
+  Ranked = 4,
+  Approved = 5,
+  Qualified = 6,
+  Loved = 7,
+}
+
 export interface SongType {
-  id: number;
-  path: string;
-  audio: string;
-
-  title: string;
-  unicodeTitle: string;
-  artist: string;
-  unicodeArtist: string;
-  mapper: string;
-
-  tags: string;
-  setId: string;
-  source: string;
-
-  audioFile: File;
+  id: number; artist_ascii: string,
+  artist_unicode: string | null,
+  title_ascii: string,
+  title_unicode: string | null,
+  creator: string | null,
+  song_source: string | null,
+  tags: string | null,
+  length: number | null,
+  mode: Mode | null,
+  ranking_status: RankedStatus | null,
+  last_modified: Date | null,
 }
